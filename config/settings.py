a91c7b6b4e434dec6c79c0cb3f517f0da6d0842c
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-12-02 21:07:23 -0300

    feat: add pythonanywhere as an allowed host

--- config/settings.py
+++ config/settings.py
@@ -29,11 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['guilhermecaz.pythonanywhere.com']
 
 # Application definition
-
 INSTALLED_APPS = [
     'posts',
     'rest_framework',
@@ -115,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
